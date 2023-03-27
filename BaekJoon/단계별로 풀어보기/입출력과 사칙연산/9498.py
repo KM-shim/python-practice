6b95458fd33455a5a693a9a14e53f081b723527e
KM-shim <rudahsanbbi6008403012@gmail.com>
2023-03-27 20:07:33 +0900

    20230327

---
+++ BaekJoon/단계별로 풀어보기/입출력과 사칙연산/9498.py
@@ -0,0 +1,12 @@
+testscore = int(input())
+
+if testscore >= 90 :
+    print('A')
+elif testscore >= 80 :
+    print('B')
+elif testscore >= 70 :
+    print('C')
+elif testscore >= 60 :
+    print('D')
+else:
+    print('F')
